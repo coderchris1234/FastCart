c3defa9cf434a50e0279ae5731d7afb921c82423
Christobel <christobelnwachukwu@gmail.com>
2025-09-16 07:30:05 +0100

    created the navbar component

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Components/Header/Navbar"
+
+
+
+function HomePage() {
+    return (
+        <div>
+           <Navbar />
+        </div>
+    )
+}
+
+export default HomePage
